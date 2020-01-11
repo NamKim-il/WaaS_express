aeb76c65e25f1036e68f6b91594a97d4f00006f0
NamKim-il <namil000208@gmail.com>
2020-01-12 00:57:08 +0900

    day2

--- 0109/routes/post.js
+++ 0109/routes/post.js
@@ -11,19 +11,46 @@ router.post('/', (req, res) => {
   
   const id = new Date().getTime().toString();
 
-  fs.writeJSONSync(postsPath, [...posts, {id: id, title: /**/'', content: /**/''}]);
+  fs.writeJSONSync(postsPath, [...posts, {id: id, title: req.body.title, content: req.body.content}]);
   
   res.status(200).json({success: true});
 });
+
 router.get('/', (req, res) => {
-  // posts.json 값 읽어서 전달
-  // 200 {success: true, posts: ~~}
-})
+  let posts = [];
+  if (fs.pathExistsSync(postsPath)) {
+    posts = fs.readJSONSync(postsPath);
+  } 
+  res.status(200).json({success: true,posts:[...post]});
+})  
+
 router.delete('/:id', (req, res) => {
-  // cookies에 auth 키에 admin이라는 value가 있을 경우
-      // posts.json에서 id가 일치하는 부분 삭제하고 저장 & 200 {success: true} 보내기
-  // 없을 경우
-      // 403 {success: false} 보내기
+  if(req.cookies.auth==='admin'){
+    let posts=[];
+    if(fs.pathExistsSync(postsPath)){
+      posts=fs.readJSONSync(postsPath);
+      let i=0;
+      let len=posts.length;
+      let index=len;
+      for(i=0;i<len;i++)
+      {
+        if(posts[i].id===req.cookies.id)
+        {
+          index=i;
+          break;
+        }
+      }
+      if(index!==len)
+      {
+        posts.splice(index,1);
+        fs.writeJSONSync(postsPath,[...posts]);
+      }
+      res.status(200).json({success: true});
+     }   
+  }
+
+  else res.status(403).json({success: false});
+
 })
 
 module.exports = router;
