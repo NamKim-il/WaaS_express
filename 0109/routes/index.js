3e874519f4d84b8d4d753aba6d9c3db9ebdb1086
NamKim-il <namil000208@gmail.com>
2020-01-12 02:16:26 +0900

    day3

--- 0109/routes/index.js
+++ 0109/routes/index.js
@@ -11,4 +11,34 @@ router.use('/err', (req, res, next) => {
   next('error occurs!');
 });
 
+
+const printChar=(alpha,time)=>{
+  return new Promise(function(resolve,reject){
+    setTimeout(()=>{
+    console.log(alpha);
+    resolve();
+  },time)
+  });
+}
+
+async function printAll(){
+  await printChar('a',5000);
+  await printChar('b',3000);
+  await printChar('c',1000);
+  throw new Error('error')
+}
+
+router.get('/async', async function(req, res, next) {
+  try {
+    await printAll();
+  } catch (err) {
+    next(err);
+  }
+});
+router.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({msg: 'error occur!'});
+});
+
+
 module.exports = router;
